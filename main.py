7265395790b2d9abb665eddf61ac19b572b433fb
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-09-08 18:56:40 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import schedule
+import time
+from twitter_wrapper import TwitterWrapper
+
+twitter = TwitterWrapper()
+a = 1
+def job():
+    global a
+    twitter.update_status("Que dia EXPLENDIDO %d" % a)
+    a += 1
+
+def main():
+    schedule.every(1).minutes.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
