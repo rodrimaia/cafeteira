24c814cb73d05477cc1f31f7538067c6ea2c01d7
Daniel Barra <dmbarra@gmail.com>
2015-09-24 13:48:22 -0300

    separa credenciais e mensagens

---
+++ src/twitter/messages.py
@@ -0,0 +1,6 @@
+class MessagesTwitter(object):
+	def get_message_making_coffee(self):
+		return "%02d:%02d começando os preparativos para um café. ThoughtWorks @ #CBSOFT2015"
+
+	def get_message_abort(self):
+		return "As %02d:%02d deu treta na cafeteira, voltamos em alguns minutos!"
