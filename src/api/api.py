888d8f515e1e32898d636bc0a1fd54369d31dc1c
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-10-01 00:58:57 -0300

    hello_api! (with tests)

---
+++ src/api/api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+class Api:
+
+    app = Flask(__name__)
+    
+    def __init__(self):
+        pass
+    
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    def start(self):
+        self.app.run()
---
+++ tests/api_test.py
@@ -0,0 +1,12 @@
+import unittest
+from api.api import Api
+import requests
+
+class ApiTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tester = Api().app.test_client(self)
+
+    def test_hello_api(self):
+        r = self.tester.get('/')
+        self.assertEqual(r.data,'Hello World!')
