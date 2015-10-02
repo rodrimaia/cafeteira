c135064db0b98eacf8d7256ed74a3effae87db77
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-10-02 14:47:20 -0300

    Daniel/Rodrigo - Adiciona tasks de testes

--- dodo.py
+++ dodo.py
@@ -1,5 +1,8 @@
 def task_test_unit():
     return { 'actions' : [ 'python tests/run_all_tests.py']  } 
 
+def task_test_unit_watch():
+    return { 'actions' : [ 'nosetests --with-watch' ] }
+
 def task_run_coffee():
     return { 'actions' : [ 'sudo src/./run' ] } 
