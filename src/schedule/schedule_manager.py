b9cc1ae5546b4eb06748f7c94f6c072ad35e9c55
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-10-05 13:57:45 -0300

    daniel/rodrigo - adiciona funcao que retorna se estamos no tempo agendado

--- src/schedule/schedule_manager.py
+++ src/schedule/schedule_manager.py
@@ -2,7 +2,14 @@ from datetime import datetime
 
 class ScheduleManager():
 
+    def __init__(self, scheduled_times):
+        self.times = scheduled_times
+
     def get_actual_time(self):
         now = datetime.now()
         return "%02d:%02d" % (now.hour,now.minute)
+
+    def its_time(self):
+        return self.get_actual_time() in self.times
+
 
--- tests/schedule_manager_test.py
+++ tests/schedule_manager_test.py
@@ -5,9 +5,17 @@ from freezegun import freeze_time
 
 class ScheduleManagerTest(unittest.TestCase):
     def setUp(self):
-        self.target = ScheduleManager()
+        self.target = ScheduleManager(['15:00','10:00'])
 
     @freeze_time("2015-10-10 10:00:00")
     def test_manager_should_format_actual_time(self):
         self.assertEqual(self.target.get_actual_time(), '10:00')
+
+    @freeze_time("2015-10-10 10:00:00")
+    def test_manager_should_return_true_if_its_scheduled_time(self):
+        self.assertTrue(self.target.its_time())
+
+    @freeze_time("2015-10-10 09:59:00")
+    def test_manager_should_return_false_if_its_scheduled_time(self):
+        self.assertFalse(self.target.its_time())
 
