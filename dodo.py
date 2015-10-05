733f33ad2b598c4cd0fa39dbbd87d63ce019b53d
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-10-05 14:20:59 -0300

    daniel/rodrigo - refatora estrutura da pasta de testes

--- dodo.py
+++ dodo.py
@@ -1,5 +1,5 @@
 def task_unit_test():
-    return { 'actions' : [ 'python tests/run_all_tests.py']  } 
+    return { 'actions' : [ 'python unit_tests/run_all_unit_tests.py']  } 
 
 def task_unit_test_watch():
     return { 'actions' : [ 'nosetests --with-watch' ] }
@@ -8,6 +8,6 @@ def task_run_coffee():
     return { 'actions' : [ 'sudo src/./run' ] } 
 
 def task_check_coverage():
-    return { 'actions' : [ 'coverage run --source=src tests/run_all_tests.py','coverage report -m' ],
+    return { 'actions' : [ 'coverage run --source=src unit_tests/run_all_unit_tests.py','coverage report -m' ],
             'verbosity' : 2
             }
--- unit_tests/run_all_unit_tests.py
+++ unit_tests/run_all_unit_tests.py
@@ -1,3 +1,2 @@
 import nose
-
 nose.main()
