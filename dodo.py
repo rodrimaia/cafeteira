f772e284c64cbde815872a50451059abd73422c6
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-10-06 15:31:57 -0300

    corrigindo erros de estilo

--- dodo.py
+++ dodo.py
@@ -1,18 +1,18 @@
 def task_unit_test():
-    return { 'actions' : [ 'nosetests -w . unit_tests']  } 
+    return { 'actions': [ 'nosetests -w . unit_tests']  }
 
 def task_unit_test_watch():
-    return { 'actions' : [ 'nosetests --with-watch -w . unit_tests' ] }
+    return { 'actions': [ 'nosetests --with-watch -w . unit_tests' ] }
 
 def task_integration_test():
-    return { 'actions' : [ 'nosetests -w . integration_tests' ] }
+    return { 'actions': [ 'nosetests -w . integration_tests' ] }
 
 def task_run_coffee():
-    return { 'actions' : [ 'sudo src/./run' ] } 
+    return { 'actions': [ 'sudo src/./run' ] } 
 
 def task_check_coverage():
-    return { 'actions' : [ 'coverage run --source=src unit_tests/run_all_unit_tests.py','coverage report -m' ],
-            'verbosity' : 2
+    return { 'actions': [ 'coverage run --source=src unit_tests/run_all_unit_tests.py','coverage report -m' ],
+            'verbosity': 2
             }
 
 def task_check_style():
--- unit_tests/machine_tests/machine_adapter_test.py
+++ unit_tests/machine_tests/machine_adapter_test.py
@@ -1,33 +1,34 @@
 import unittest
 from machine.machine_adapter import MachineAdapter
 
+
 class MachineTest(unittest.TestCase):
 
     def setUp(self):
         self.machine = MachineAdapter()
 
     def test_machine_relay_should_start_turned_off(self):
-        assert self.machine.is_relay_on() == False
+        self.assertFalse(self.machine.is_relay_on())
 
     def test_start_should_turn_relay_on(self):
         self.machine.start()
-        assert self.machine.is_relay_on() == True
+        self.assertTrue(self.machine.is_relay_on())
 
     def test_stop_should_turn_relay_off(self):
         self.machine.stop()
-        assert self.machine.is_relay_on() == False
+        self.assertFalse(self.machine.is_relay_on())
 
     def test_toggle_should_alternate_relay_values(self):
         self.machine.toggle()
-        assert self.machine.is_relay_on() == True
+        self.assertTrue(self.machine.is_relay_on())
         self.machine.toggle()
-        assert self.machine.is_relay_on() == False
+        self.assertFalse(self.machine.is_relay_on())
 
     def test_button_toggle_machine(self):
         self.machine.buttonHandler()
-        assert self.machine.is_relay_on() == True
+        self.assertTrue(self.machine.is_relay_on())
 
     def test_button_toggle_machine_stopped(self):
         self.machine.start()
         self.machine.buttonHandler()
-        self.assertEqual(self.machine.is_relay_on(),False)
+        self.assertFalse(self.machine.is_relay_on())
