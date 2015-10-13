02ffae51cc260fb3876f99320043e8c8e769e118
Daniel Barra <dmbarra@gmail.com>
2015-10-13 14:16:34 -0300

    move controle da thread para arquivo da cafeteira

--- src/cafeteira.py
+++ src/cafeteira.py
@@ -19,17 +19,13 @@ class Cafeteira:
             if (self.schedule.its_time() and
                     self.machine.machine_status == MachineStatus.stand_by):
                 print 'horario confere com agendado'
-                self.machine.make_coffee()
-                self.machine.keep_coffee_hot()
-                self.machine.go_back_stand_by()
+                self.machine.start_coffee_routine()
             time.sleep(59)
 
     def button_callback(self, pin):
         print "botao acionado"
         if (self.machine.machine_status == MachineStatus.stand_by):
-            self.machine.make_coffee()
-            self.machine.keep_coffee_hot()
-            self.machine.go_back_stand_by()
+            self.machine.start_coffee_routine()
         else:
             self.machine.interrupt()
 
@@ -38,3 +34,9 @@ class Cafeteira:
         reader = ScheduleReader(FILE_PATH)
         times = reader.read_scheduled_times()
         return ScheduleManager(times)
+
+    def start_coffee_routine_async(self):
+        if self.machine.current_action is None:
+            thread_making_coffee = Process(target=self.machine.start_coffee_routine())
+            thread_making_coffee.start()
+            self.machine.current_action = thread_making_coffee
